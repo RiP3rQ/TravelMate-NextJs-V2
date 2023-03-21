f7041480f8494100eb2916b5de6c215d9317a525
RiP3rQ <rafalpompa2000@gmail.com>
2023-03-21 21:37:28 +0100

    Image fix in medium card

--- components/MediumCard.jsx
+++ components/MediumCard.jsx
@@ -5,7 +5,7 @@ const MediumCard = ({ img, title }) => {
   return (
     <div className="cursor-pointer hover:scale-105 transition transform duration-300 ease-out">
       <div className="relative h-80 w-80">
-        <Image src={img} fill className="rounded-xl" />
+        <Image src={img} alt={title} fill className="rounded-xl" />
       </div>
       <h3 className="text-2xl mt-3 truncate">{title}</h3>
     </div>
